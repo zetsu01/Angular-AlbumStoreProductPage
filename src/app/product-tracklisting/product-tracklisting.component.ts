fa3aac870dcbf55a7d3df9650e658c239a04aabb
admincarmes <admincarmes@groupasma.local>
2019-10-23 13:34:08 +0100

    Completed module

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,19 @@
+import { Album } from '../album';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
+})
+export class ProductTracklistingComponent implements OnInit {
+albumInfo: Album;
+
+  constructor(private _productService: ProductService) { }
+
+  ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
+  }
+
+}

6092499878c28f79dad3bbc72bbab2ad86753252
admincarmes <admincarmes@groupasma.local>
2019-10-23 13:46:27 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,11 +3,13 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
-  private _albumUrl = "../assets/album.json"
+  private _albumUrl = "../assets/album.json";
+  private _productsUrl = "../assets/products.json";
 
   constructor(private _http: Http) { }
 
@@ -15,4 +17,7 @@ export class ProductService {
    return this._http.get(this._albumUrl).map((response) => <Album>response.json())
   }
 
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json())
+   }
 }

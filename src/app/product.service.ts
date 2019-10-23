2c15b30c6b029bf47918941e5bbb0773dd3bed11
admincarmes <admincarmes@groupasma.local>
2019-10-23 12:56:50 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,7 +9,6 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-
   getAlbum(id: number) {
    return this._http.get(this._albumUrl).map((response) => {
       response.json();
